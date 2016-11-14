83e8cd2e8353b6346587d3e9dc2630ba32defa52
satej1210 <satej1210@live.com>
2016-11-14 15:37:01 -0600

    Added new stuff

--- src/GameManager.java
+++ src/GameManager.java
@@ -6,23 +6,68 @@ import DDS.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class GameManager {
-    public static boolean playerJoined = false;
-    public static boolean dealerJoined = false;
-    public static boolean playerRunning = true;
-    public static boolean dealerRunning = true;
     private static final String dealerInit = "Dealer_init";
     private static final String playerInit = "Player_init";
-
-
-    public static ArrayList<bjPlayer> players = null;
-    public static ArrayList<bjDealer> dealers = null;
+    public boolean playerJoined = false;
+    public boolean dealerJoined = false;
+    public boolean playerRunning = true;
+    public boolean dealerRunning = true;
+    public ArrayList<bjPlayer> players = null;
+    public ArrayList<bjDealer> dealers = null;
+    public ArrayList<Table> tables = null;
+    UUID id;
+    private boolean playerInitTh = false, dealerInitTh = false, player = false, dealer = false;
     GameManager(){
+        id = UUID.randomUUID();
         players = new ArrayList<>();
         dealers = new ArrayList<>();
+        tables = new ArrayList<>();
     }
-    public static void initializePlayer(){
+
+    public static int Start() {
+        System.out.println("Press Enter to start Casino Royale...");
+        Scanner s = new Scanner(System.in);
+        String f = s.nextLine();
+        System.out.println("1. Start New Game");
+        System.out.println("2. Exit\nEnter:");
+        return s.nextInt();
+    }
+
+    public static void NewGame(GameManager n) {
+        n.players = new ArrayList<>();
+        n.dealers = new ArrayList<>();
+        n.initializeDealer();
+        n.initializePlayer();
+        n.subscribe();
+        while (n.playerRunning || n.dealerRunning) {
+            if (n.playerRunning && n.dealerRunning) {
+                n.DisplayMenu();
+            }
+        }
+    }
+
+    public static void main(String... args) {
+        int opt = Start();
+        while (true) {
+            switch (opt) {
+                case 1:
+                    GameManager n = new GameManager();
+                    NewGame(n);
+                    break;
+                case 2:
+                    System.exit(0);
+                default:
+                    System.out.println("Incorrect option");
+            }
+        }
+
+
+    }
+
+    public void initializePlayer() {
         DDSEntityManager mgr = new DDSEntityManager();
 
         // create Domain Participant
@@ -51,12 +96,12 @@ public class GameManager {
 
         SampleInfoSeqHolder infoSeq = new SampleInfoSeqHolder();
 
-        System.out.println ("=== [Subscriber] Ready to receive player init ...");
+        System.out.println("From GM " + id + " === [Subscriber] Ready to receive player init ...");
         Runnable backGroundRunnable = () -> {
 
             boolean terminate = false;
             int count = 0;
-            while (!terminate) { // We dont want the example to run indefinitely
+            while (!playerInitTh) { // We dont want the example to run indefinitely
                 bjPlayerReader.take(bjPlayerSeq, infoSeq, LENGTH_UNLIMITED.value,
                         ANY_SAMPLE_STATE.value, ANY_VIEW_STATE.value,
                         ANY_INSTANCE_STATE.value);
@@ -67,14 +112,19 @@ public class GameManager {
                         boolean exists = false;
                         for(bjPlayer p : players){
                             if(p.uuid == bjPlayerSeq.value[i].uuid){
-                                System.out.println("Player has already joined.");
+                                System.out.println("From GM " + id + " Player has already joined.");
                                 exists = true;
                                 break;
                             }
                         }
                         if(!exists){
-                            System.out.println("=== [Subscriber] message received from new Player:");
+                            System.out.println("From GM " + id + " === [PlayerSubscriber] message received from new Player:");
                             System.out.println("A Player with UUID " + bjPlayerSeq.value[i].uuid + " has joined!");
+                            players.add(bjPlayerSeq.value[i]);
+                        }
+                        if (!dealerJoined) {
+                            System.out.println("From GM " + id + " === [PlayerSubscriber] WARNING!!!:");
+                            System.out.println("Dealer has not joined yet. Will wait for dealer to join.");
                         }
                         playerJoined = true;
                     }
@@ -100,12 +150,14 @@ public class GameManager {
             mgr.deleteSubscriber();
             mgr.deleteTopic();
             mgr.deleteParticipant();
-
+            System.out.println("Stopping Thread");
         };
         Thread sampleThread = new Thread(backGroundRunnable);
+
         sampleThread.start();
     }
-    public static void initializeDealer(){
+
+    public void initializeDealer() {
         DDSEntityManager mgr = new DDSEntityManager();
 
         // create Domain Participant
@@ -134,12 +186,12 @@ public class GameManager {
 
         SampleInfoSeqHolder infoSeq = new SampleInfoSeqHolder();
 
-        System.out.println ("=== [Subscriber] Ready to receive dealer init ...");
+        System.out.println("From GM " + id + " === [Subscriber] Ready to receive dealer init ...");
         Runnable backGroundRunnable = () -> {
 
             boolean terminate = false;
             int count = 0;
-            while (!terminate) { // We dont want the example to run indefinitely
+            while (!dealerInitTh) { // We dont want the example to run indefinitely
                 bjDealerReader.take(bjDealerSeq, infoSeq, LENGTH_UNLIMITED.value,
                         ANY_SAMPLE_STATE.value, ANY_VIEW_STATE.value,
                         ANY_INSTANCE_STATE.value);
@@ -149,16 +201,24 @@ public class GameManager {
                         boolean exists = false;
                         for(bjDealer p : dealers){
                             if(p.uuid == bjDealerSeq.value[i].uuid){
-                                System.out.println("Dealer has already joined.");
+                                System.out.println("From GM " + id + " Dealer has already joined.");
                                 exists = true;
                                 break;
                             }
                         }
                         if(!exists){
-                            System.out.println("=== [Subscriber] message received from new Dealer:");
+                            System.out.println("From GM " + id + " === [DealerInitSubscriber] message received from new Dealer:");
                             System.out.println("A Dealer with UUID " + bjDealerSeq.value[i].uuid + " has joined!");
+                            dealers.add(bjDealerSeq.value[i]);
+                            for (bjPlayer p : players) {
+                                if (p.dealer_id == 0) {
+                                    AssignTable(bjDealerSeq.value[i], null);
+                                }
+                            }
                         }
-                        System.out.println("=== [Subscriber] message received from new Dealer :");
+                        System.out.println("From GM " + id + " Now assigning Table to dealer: ");
+                        AssignTable(bjDealerSeq.value[i], null);
+                        System.out.println("From GM " + id + " === [DealerInitSubscriber] message received from new Dealer :");
                         System.out.println("Dealer is now waiting...");
                         dealerJoined = true;
 
@@ -186,12 +246,14 @@ public class GameManager {
             mgr.deleteSubscriber();
             mgr.deleteTopic();
             mgr.deleteParticipant();
-
+            System.out.println("Stopping Thread");
         };
         Thread sampleThread = new Thread(backGroundRunnable);
+
         sampleThread.start();
     }
-    public static void SubscribeToPlayer(){
+
+    public void SubscribeToPlayer() {
         DDSEntityManager mgr = new DDSEntityManager();
         String partitionName = "Player";
 
@@ -219,18 +281,18 @@ public class GameManager {
         bjPlayerSeqHolder bjPlayerSeq = new bjPlayerSeqHolder();
         SampleInfoSeqHolder infoSeq = new SampleInfoSeqHolder();
 
-        System.out.println ("=== [Subscriber] Ready ...");
+        System.out.println("From GM " + id + " === [Subscriber] Ready for Player ...");
         Runnable backGroundRunnable = () -> {
 
             boolean terminate = false;
             int count = 0;
-            while (!terminate) { // We dont want the example to run indefinitely
+            while (!player) { // We dont want the example to run indefinitely
                 bjPlayerReader.take(bjPlayerSeq, infoSeq, LENGTH_UNLIMITED.value,
                         ANY_SAMPLE_STATE.value, ANY_VIEW_STATE.value,
                         ANY_INSTANCE_STATE.value);
                 for (int i = 0; i < bjPlayerSeq.value.length; i++) {
                     if (bjPlayerSeq.value[i].getClass() == bjPlayer.class){//.message.equals("Hello World")) {
-                        System.out.println("=== [Subscriber] message received from Player :");
+                        System.out.println("From GM " + id + " === [Subscriber] message received from Player :");
                         System.out.println("    uuid  : "
                                 + bjPlayerSeq.value[i].uuid);
                         System.out.println("    Message : \""
@@ -252,7 +314,7 @@ public class GameManager {
 
             }
             bjPlayerReader.return_loan(bjPlayerSeq, infoSeq);
-
+            System.out.println("Stopping Thread");
             // clean up
             mgr.getSubscriber().delete_datareader(bjPlayerReader);
             mgr.deleteSubscriber();
@@ -261,9 +323,41 @@ public class GameManager {
 
         };
         Thread sampleThread = new Thread(backGroundRunnable);
+
         sampleThread.start();
     }
-    public static void SubscribeToDealer(){
+
+    public void AssignTable(bjDealer d, bjPlayer p) {
+        if (p == null && d != null) {
+            tables.add(new Table(d));
+            System.out.println("Dealer with uuid " + d.uuid + " assigned to Table with uuid " + tables.get(tables.size() - 1).getUUID());
+        }
+        if (d == null && p != null) {
+            for (Table t : tables) {
+                if (t.players.size() < 6) {
+                    p.dealer_id = t.dealer.uuid;
+                    t.players.add(p);
+                    System.out.println("From GM " + id + " === Player with uuid " + p.uuid + " added to Table with uuid " + t.getUUID() + " with Dealer with uuid " + t.dealer.uuid);
+
+                    break;
+                }
+            }
+        }
+//        if(d!=null && p!=null){
+//            for(Table table: tables) {
+//                if (table.players.size() >= 6) {
+//                    continue;
+//                }
+//                else{
+//                    p.dealer_id = d.uuid;
+//                    tables.add(new Table(d, p));
+//                }
+//            }
+//
+//        }
+    }
+
+    public void SubscribeToDealer() {
         DDSEntityManager mgr = new DDSEntityManager();
         String partitionName = "Dealer";
 
@@ -291,18 +385,18 @@ public class GameManager {
         bjDealerSeqHolder bjDealerSeq = new bjDealerSeqHolder();
         SampleInfoSeqHolder infoSeq = new SampleInfoSeqHolder();
 
-        System.out.println ("=== [Subscriber] Ready ...");
+        System.out.println("From GM " + id + " === [Subscriber] Ready ...");
         Runnable backGroundRunnable = () -> {
 
             boolean terminate = false;
             int count = 0;
-            while (!terminate) { // We dont want the example to run indefinitely
+            while (!dealer) { // We dont want the example to run indefinitely
                 bjDealerReader.take(bjDealerSeq, infoSeq, LENGTH_UNLIMITED.value,
                         ANY_SAMPLE_STATE.value, ANY_VIEW_STATE.value,
                         ANY_INSTANCE_STATE.value);
                 for (int i = 0; i < bjDealerSeq.value.length; i++) {
                     if (bjDealerSeq.value[i].getClass() == bjDealer.class){//.message.equals("Hello World")) {
-                        System.out.println("=== [Subscriber] message received from Dealer :");
+                        System.out.println("From GM " + id + " === [Subscriber] message received from Dealer :");
                         System.out.println("    uuid  : "
                                 + bjDealerSeq.value[i].uuid);
                         System.out.println("    Message : \""
@@ -328,17 +422,13 @@ public class GameManager {
             mgr.deleteSubscriber();
             mgr.deleteTopic();
             mgr.deleteParticipant();
-
+            System.out.println("Stopping Thread");
         };
         Thread sampleThread = new Thread(backGroundRunnable);
         sampleThread.start();
     }
-    public static void Start(){
-//        System.out.println("Press Enter to start Casino Royale...");
-//        Scanner s = new Scanner(System.in);
-//        String f = s.nextLine();
-    }
-    public static void subscribe(){
+
+    public void subscribe() {
         Runnable backGroundRunnable = () -> {
             boolean a = true;
             while(a) {
@@ -350,13 +440,17 @@ public class GameManager {
         Thread sampleThread = new Thread(backGroundRunnable);
         sampleThread.start();
     }
-    public static void main(String... args) {
-        players = new ArrayList<>();
-        dealers = new ArrayList<>();
-        Start();
-        initializeDealer();
-        initializePlayer();
-        subscribe();
-        while(playerRunning || dealerRunning);
+
+    public void DisplayMenu() {
+        System.out.println("From GM: " + id + " === Player and Dealer Threads running in background.");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("1. Exit GM " + id + "\nEnter:");
+        int f = s.nextInt();
+        switch (f) {
+            case 1:
+                System.out.println("Stopping all Threads:");
+        }
+
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,39 @@
+import CR.bjDealer;
+import CR.bjPlayer;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  * Created by Satej on 11/14/2016.
  */
 public class Table {
+    UUID u;
+    bjDealer dealer;
+    ArrayList<bjPlayer> players;
+
+    Table(bjDealer d) {
+        dealer = d;
+    }
+
+    Table(bjDealer d, bjPlayer p) {
+        if (u == null)
+            u = UUID.randomUUID();
+        dealer = d;
+        if (players == null)
+            players = new ArrayList<>();
+        CheckSize();
+        players.add(p);
+
+    }
+
+    UUID getUUID() {
+        return u;
+    }
+
+    void CheckSize() {
+        if (players.size() >= 6) {
+            System.out.println("Players full");
+        }
+    }
 }
